3968ad96d5b2f68d080282e397c1fe7749b708d1
salinisebastian <salini.seb@gmail.com>
2021-03-21 23:09:42 +0100

    logout test

---
+++ cypress/integration/secondTest.spec.js
@@ -0,0 +1,11 @@
+describe("Test log out", () => {
+  beforeEach("login to the app", () => {
+    cy.LoginToApplication();
+  });
+
+  it("verify use can log out successfully", () => {
+    cy.contains("Settings").click();
+    cy.contains("Or click here to logout").click();
+    cy.get(".navbar-nav").should("contain", "Sign up");
+  });
+});
